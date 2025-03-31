33b36b09e450186524353e71c9a4bdaaa2351cd2
LeeTaeYoung <xodud5080@naver.com>
2025-03-31 12:51:55 +0900

    zxc

--- src/quiz/data/front_data.js
+++ src/quiz/data/front_data.js
@@ -1,120 +1,124 @@
 export const frontTechnologyData = [
     {
-        question: "프로그래밍이란?",
-        answer: "컴퓨터에게 특정 작업을 수행하도록 명령하는 과정으로\n 알고리즘 설계, 코드작성, 디버깅 및 테스트를 포함"
-    },
-    {
-        question: "브라우저의 렌더링 원리",
-        answer: "HTML과 CSS를 해석하여 DOM과 CSSOM을 생성하고,\n 이를 결합해 렌더 트리를 만들고, 이를 기반으로 화면에 표시"
-    },
-    {
-        question: "라이브러리란 개념",
-        answer: "특정 기능을 수행하는 코드 모음으로,\n 개발자가 필요할 때 호출하여 가능"
-    },
-    {
-        question: "프레임워크란 개념",
-        answer: "애플리케이션 구조를 제공하여,\n특정 규칙을 따르도록 강제하며,\n 전체 애플리케이션 흐름을 관리"
-    },
-    {
-        question: "주소창에 google.com을 입력하면 일어나는 일",
-        answer: "DNS 조회를 통해 IP 주소를 찾고, 서버와 통신하여 받은\n HTML, CSS, JavaScript 등을 렌더링해 화면에 표시"
-    },
-    {
-        question: "position(static) 개념",
-        answer: "요소를 일반적인 문서 흐름에 따라 배치,\n 오프셋(top, right, bottom, left) 적용 불가능"
-    },
-    {
-        question: "position(relative) 개념",
-        answer: "static + 자신 기준으로 오프셋(top, right, bottom, left) 적용"
-    },
-    {
-        question: "position(fixed) 개념",
-        answer: "뷰포트를 기준으로 고정, 항상 같은 위치에 머물도록 지정"
-    },
-    {
-        question: "position(sticky) 개념",
-        answer: "스크롤을 통해 일정 스크롤구간에서는 relative처럼 동작하다,\n 설정된 임계점 이후에는 fixed로 동작한다."
-    },
-    {
-        question: "position(absolute) 개념",
-        answer: "요소를 일반적인 문서 흐름을 제거,\n가장 가까운 부모 요소나 조상 요소 기준으로 정확한 위치 지정"
-    },
-    {
-        question: "API 개념",
-        answer: "소프트웨어 간의 상호작용을 가능하게 하는 프로토콜로,\n 다양한 서비스나 기능을 사용할 수 있도록 도와주는 역할"
-    },
-    {
-        question: "Rest API, Restful API 공통점",
-        answer: "HTTP 프로토콜을 사용해, CRUD 작업을 수행하는 인터페이스 \n (GET = 기존 자원을 조회, POST = 새로운 자원을 생성, DELETE = 기존 자원을 삭제, PUT = 기존 자원을 수정)"
-    },
-    {
-        question: "Rest API, Restful API 차이점",
-        answer: "Rest API는 REST규칙을 준수하지 않을 수 도 있지만, \n Restful API는 REST규칙을 엄격히 준수해야한다 \n(Rest API > Restful API)"
-    },
-    {
-        question: "REST 규칙",
-        answer: "클라이언트와 서버는 독립적으로 존재, \n상호작용은 일관된 인터페이스를 통해 이루어져야한다"
-    },
-    {
-        question: "AJAX 개념",
-        answer: "자바스크립트를 통해 서버와 클라이언트가 비동기 방식으로\n데이터를 교환할 수 있는 통신기능"
-    },
-    {
-        question: "OAuth 개념",
-        answer: "인터넷 사용자가 비밀번호를 제공하지 않고,\n 웹사이트를 이용할 수 있도록 하는 프로토콜"
-    },
-    {
-        question: "URI, URL 차이점",
-        answer: "URI = 자원을 식별(포괄적인 개념)\n URL = 자원의 위치를 나타냄(https://www.naver.com)\n(URI > URL)"
-    },
-    {
-        question: "CORS 개념",
-        answer: "다른 출처의 리소스에 안전하게 접근할 수 있도록 돕는 역할\n(서버에서 사용)"
-    },
-    {
-        question: "meta 개념 및 종류",
-        answer: "메타 데이터의 줄임말 \n charset(문자 인코딩 정의) => UTF=8 \n name(메타데이터 종류 정의) => description | keyword \n viewport(반응형 웹 디자인을 위한 설정) => initial-scale=1.0"
-    },
-    {
-        question: "PX, EM 정의",
-        answer: "PX = 절대 단위로 고정된 픽셀 수로 나타낸다 \n EM = 상대 단위로 현재 요소의 글꼴 크기를 기준으로 정의"
-    },
-    {
-        question: "반응형 웹의 3요소",
-        answer: "유연한 레이아웃 = 유연한 단위를 사용하여,\n 다양한 화면 크기에 자동 조정 되도록 설정 \n\n 유연한 이미지 = max-width: 100%를 사용하여,\n 이미지가 부모 요소의 크기를 초과하지 않도록 설정 \n\n 미디어 쿼리 = @media 규칙을 사용하여,\n 다양한 디바이스에 맞게 스타일을 적용하는 역할"
-    },
-    {
-        question: "CSS 적용 우선순위",
-        answer: "1) HTML요소에 style속성에 직접 작성 (인라인 스타일) \n 2) ID를 선택하여 style 적용 (ID 선택자) \n 3) 클래스를 선택하여 style 적용 (클래스 선택자) \n 4) 태그를 선택하여 style 적용 (태그 선택자) \n 5) 유니버설(*)을 선택하여 style 적용 (유니버설 선택자) \n * !important를 사용하여 우선순위를 높일 수 있음 *"
-    },
-    {
-        question: "브라우저 저장소(LocalStorage, SessionStorage, Cookie)의 차이점",
-        answer: "LocalStorage = 영구적으로 보관 가능(자동 로그인) \n\n SessionStorage = 브라우저 종료시 삭제(비로그인 활동) \n\n Cookie = 만료일 설정 시 유지, 브라우저 종료시 삭제(팝업 창)"
-    },
-    {
-        question: "null, undefined, undeclared, NaN 개념",
-        answer: "null = 빈값 \n\n undefined = 정의(변수에 값을 할당)되지 않음 \n\n undeclared = 선언(변수를 만드는 것)되지 않음 \n\n NaN = 표현 불가능한 수치형 결과"
-    },
-    {
-        question: "HTML 렌더링 중에 JavaScript가 실행되면 렌더링이 멈추는 이유",
-        answer: "JavaScript에게 엔진 제어권을 넘겨서"
-    },
-    {
-        question: "scope 개념 및 종류",
-        answer: "프로그램에서 접근 가능한 범위를 정의 \n\n Global Scope (전역 스코프) = 프로그램 전체에서 접근 가능 \n\n Function Scope (함수 스코프) = 함수 내에서만 접근 가능 \n\n Block Scope (블록 스코프) = {} 묶인 블록 내에서만 접근 가능 \n\n Lexical Scope (어휘적 스코프) = 함수가 선언된 위치에 따라 접근 범위가 정해진다 \n\n Module Scope (모듈 스코프) = 각 모듈 파일이 독립적으로\n 범위를 가지도록 설정, 다른 모듈에서는 접근 불가능"
-    },
-    {
-        question: "DOM 개념 및 종류",
-        answer: "웹페이지의 구조와 요소를 트리 형태로 표현하는 객체 모델\n (JavaScript를 통한 API) \n\n Real DOM = 실제로 사용자에게 보여지는 DOM\n 업데이트 비용이 크고, UI변경이 느림 \n\n Virtual DOM = 변화가 있을때 효율적으로 처리하기 위한 기술 \n 빠른 렌더링, 효율적인 업데이트"
-    },
-    {
-        question: "FLUX 개념",
-        answer: "단방향 데이터 흐름을 통해 상태 관리를 단순화하고,\n 예측가능하게 도와주는 역할"
-    },
-    {
-        question: "Reflow와 Repaint가 실행되는 시점",
-        answer: "Reflow가 끝나고 Repaint가 실행된다"
-    },
+        question : "자기소개",
+        answer : ""
+    }
+    // {
+    //     question: "프로그래밍이란?",
+    //     answer: "컴퓨터에게 특정 작업을 수행하도록 명령하는 과정으로\n 알고리즘 설계, 코드작성, 디버깅 및 테스트를 포함"
+    // },
+    // {
+    //     question: "브라우저의 렌더링 원리",
+    //     answer: "HTML과 CSS를 해석하여 DOM과 CSSOM을 생성하고,\n 이를 결합해 렌더 트리를 만들고, 이를 기반으로 화면에 표시"
+    // },
+    // {
+    //     question: "라이브러리란 개념",
+    //     answer: "특정 기능을 수행하는 코드 모음으로,\n 개발자가 필요할 때 호출하여 가능"
+    // },
+    // {
+    //     question: "프레임워크란 개념",
+    //     answer: "애플리케이션 구조를 제공하여,\n특정 규칙을 따르도록 강제하며,\n 전체 애플리케이션 흐름을 관리"
+    // },
+    // {
+    //     question: "주소창에 google.com을 입력하면 일어나는 일",
+    //     answer: "DNS 조회를 통해 IP 주소를 찾고, 서버와 통신하여 받은\n HTML, CSS, JavaScript 등을 렌더링해 화면에 표시"
+    // },
+    // {
+    //     question: "position(static) 개념",
+    //     answer: "요소를 일반적인 문서 흐름에 따라 배치,\n 오프셋(top, right, bottom, left) 적용 불가능"
+    // },
+    // {
+    //     question: "position(relative) 개념",
+    //     answer: "static + 자신 기준으로 오프셋(top, right, bottom, left) 적용"
+    // },
+    // {
+    //     question: "position(fixed) 개념",
+    //     answer: "뷰포트를 기준으로 고정, 항상 같은 위치에 머물도록 지정"
+    // },
+    // {
+    //     question: "position(sticky) 개념",
+    //     answer: "스크롤을 통해 일정 스크롤구간에서는 relative처럼 동작하다,\n 설정된 임계점 이후에는 fixed로 동작한다."
+    // },
+    // {
+    //     question: "position(absolute) 개념",
+    //     answer: "요소를 일반적인 문서 흐름을 제거,\n가장 가까운 부모 요소나 조상 요소 기준으로 정확한 위치 지정"
+    // },
+    // {
+    //     question: "API 개념",
+    //     answer: "소프트웨어 간의 상호작용을 가능하게 하는 프로토콜로,\n 다양한 서비스나 기능을 사용할 수 있도록 도와주는 역할"
+    // },
+    // {
+    //     question: "Rest API, Restful API 공통점",
+    //     answer: "HTTP 프로토콜을 사용해, CRUD 작업을 수행하는 인터페이스 \n (GET = 기존 자원을 조회, POST = 새로운 자원을 생성, DELETE = 기존 자원을 삭제, PUT = 기존 자원을 수정)"
+    // },
+    // {
+    //     question: "Rest API, Restful API 차이점",
+    //     answer: "Rest API는 REST규칙을 준수하지 않을 수 도 있지만, \n Restful API는 REST규칙을 엄격히 준수해야한다 \n(Rest API > Restful API)"
+    // },
+    // {
+    //     question: "REST 규칙",
+    //     answer: "클라이언트와 서버는 독립적으로 존재, \n상호작용은 일관된 인터페이스를 통해 이루어져야한다"
+    // },
+    // {
+    //     question: "AJAX 개념",
+    //     answer: "자바스크립트를 통해 서버와 클라이언트가 비동기 방식으로\n데이터를 교환할 수 있는 통신기능"
+    // },
+    // {
+    //     question: "OAuth 개념",
+    //     answer: "인터넷 사용자가 비밀번호를 제공하지 않고,\n 웹사이트를 이용할 수 있도록 하는 프로토콜"
+    // },
+    // {
+    //     question: "URI, URL 차이점",
+    //     answer: "URI = 자원을 식별(포괄적인 개념)\n URL = 자원의 위치를 나타냄(https://www.naver.com)\n(URI > URL)"
+    // },
+    // {
+    //     question: "CORS 개념",
+    //     answer: "다른 출처의 리소스에 안전하게 접근할 수 있도록 돕는 역할\n(서버에서 사용)"
+    // },
+    // {
+    //     question: "meta 개념 및 종류",
+    //     answer: "메타 데이터의 줄임말 \n charset(문자 인코딩 정의) => UTF=8 \n name(메타데이터 종류 정의) => description | keyword \n viewport(반응형 웹 디자인을 위한 설정) => initial-scale=1.0"
+    // },
+    // {
+    //     question: "PX, EM 정의",
+    //     answer: "PX = 절대 단위로 고정된 픽셀 수로 나타낸다 \n EM = 상대 단위로 현재 요소의 글꼴 크기를 기준으로 정의"
+    // },
+    // {
+    //     question: "반응형 웹의 3요소",
+    //     answer: "유연한 레이아웃 = 유연한 단위를 사용하여,\n 다양한 화면 크기에 자동 조정 되도록 설정 \n\n 유연한 이미지 = max-width: 100%를 사용하여,\n 이미지가 부모 요소의 크기를 초과하지 않도록 설정 \n\n 미디어 쿼리 = @media 규칙을 사용하여,\n 다양한 디바이스에 맞게 스타일을 적용하는 역할"
+    // },
+    // {
+    //     question: "CSS 적용 우선순위",
+    //     answer: "1) HTML요소에 style속성에 직접 작성 (인라인 스타일) \n 2) ID를 선택하여 style 적용 (ID 선택자) \n 3) 클래스를 선택하여 style 적용 (클래스 선택자) \n 4) 태그를 선택하여 style 적용 (태그 선택자) \n 5) 유니버설(*)을 선택하여 style 적용 (유니버설 선택자) \n * !important를 사용하여 우선순위를 높일 수 있음 *"
+    // },
+    // {
+    //     question: "브라우저 저장소(LocalStorage, SessionStorage, Cookie)의 차이점",
+    //     answer: "LocalStorage = 영구적으로 보관 가능(자동 로그인) \n\n SessionStorage = 브라우저 종료시 삭제(비로그인 활동) \n\n Cookie = 만료일 설정 시 유지, 브라우저 종료시 삭제(팝업 창)"
+    // },
+    // {
+    //     question: "null, undefined, undeclared, NaN 개념",
+    //     answer: "null = 빈값 \n\n undefined = 정의(변수에 값을 할당)되지 않음 \n\n undeclared = 선언(변수를 만드는 것)되지 않음 \n\n NaN = 표현 불가능한 수치형 결과"
+    // },
+    // {
+    //     question: "HTML 렌더링 중에 JavaScript가 실행되면 렌더링이 멈추는 이유",
+    //     answer: "JavaScript에게 엔진 제어권을 넘겨서"
+    // },
+    // {
+    //     question: "scope 개념 및 종류",
+    //     answer: "프로그램에서 접근 가능한 범위를 정의 \n\n Global Scope (전역 스코프) = 프로그램 전체에서 접근 가능 \n\n Function Scope (함수 스코프) = 함수 내에서만 접근 가능 \n\n Block Scope (블록 스코프) = {} 묶인 블록 내에서만 접근 가능 \n\n Lexical Scope (어휘적 스코프) = 함수가 선언된 위치에 따라 접근 범위가 정해진다 \n\n Module Scope (모듈 스코프) = 각 모듈 파일이 독립적으로\n 범위를 가지도록 설정, 다른 모듈에서는 접근 불가능"
+    // },
+    // {
+    //     question: "DOM 개념 및 종류",
+    //     answer: "웹페이지의 구조와 요소를 트리 형태로 표현하는 객체 모델\n (JavaScript를 통한 API) \n\n Real DOM = 실제로 사용자에게 보여지는 DOM\n 업데이트 비용이 크고, UI변경이 느림 \n\n Virtual DOM = 변화가 있을때 효율적으로 처리하기 위한 기술 \n 빠른 렌더링, 효율적인 업데이트"
+    // },
+    // {
+    //     question: "FLUX 개념",
+    //     answer: "단방향 데이터 흐름을 통해 상태 관리를 단순화하고,\n 예측가능하게 도와주는 역할"
+    // },
+    // {
+    //     question: "Reflow와 Repaint가 실행되는 시점",
+    //     answer: "Reflow가 끝나고 Repaint가 실행된다"
+    // },
 ];
 
 export const frontTechnologyHtmlData = [
